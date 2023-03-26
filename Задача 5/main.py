c18bc38eb9679a5851129886e6d77deab18908f8
Ghukii <68664943+Ghukii@users.noreply.github.com>
2023-03-26 13:11:52 +0300

    Task 5 main

--- Задача 5/main.py
+++ Задача 5/main.py
@@ -4,39 +4,46 @@ import numpy as np
 img = Image.open("C:/Users/Admin/Documents/GitHub/Project-python-tasks/Задача 5/Image.png")
 
 print("Введите фразу для шифрования:")
-phrase = list(input())
+phrase = list(map(lambda x: bin(ord(x))[2:], input()))
+
+def parameter(x):
+    if len(x) < 8:
+        print(x)
+        x = ('0' * (8 - len(x))) + x
+        print(x)
+    return x
+
+phrase = list(map(lambda x: parameter(x), phrase))
+
 print(phrase)
+delta = list()
+for i in phrase:
+    for j in range(0, len(i)-2, 2):
+        delta.append(i[j] + i[j + 1])
 
-obj = img.load()
-(weight, height) = img.size
+def rgb_to_hex(r, g, b):
+    return ('{:x}{:x}{:x}').format(r, g, b)
 
-def code_letter_in_pixel(pixel, letter):
-    new_pixel = list()
-    delta1, delta2 = ord(letter) // 3, ord(letter) // 3
-    delta3 = ord(letter) - delta1 - delta2
-    if pixel[0] >= 127:
-        new_pixel.append(pixel[0] - delta1)
-    else:
-        new_pixel.append(pixel[0] - delta1)
-    if pixel[0] >= 127:
-        new_pixel.append(pixel[1] - delta2)
-    else:
-        new_pixel.append(pixel[1] - delta2)
-    if pixel[0] >= 127:
-        new_pixel.append(pixel[2] - delta3)
-    else:
-        new_pixel.append(pixel[2] - delta3)
-    return tuple(new_pixel)
+def hex_to_rgb(hex):
+    return tuple(int(hex[i:i+2], 16) for i in (2, 4, 6))
 
-i = 0
-j = 0
-while phrase:
-    letter = phrase.pop(0)
-    img.putpixel((i, j), code_letter_in_pixel(img.getpixel((i, j)), letter))
-    j += 1
-    if j > weight:
-        i += 1
-        j = 0
+def shifr(delta):
+    width, height = img.size
+    x = 0
+    y = 0
+    while delta:
+        d = delta.pop(0)
+        r, g, b = img.getpixel((x, y))
+    
+        hexx = hex_to_rgb(hex(int(rgb_to_hex(r, g, b), 16) + int(d, 2)))
+        img.putpixel((x, y), hexx)
+        x += 1
+        if x > width:
+            x = 0
+            y += 1
+    return img
 
-img.save("coded_inage.png")
+def save_img(img):
+    img.save(f"coded_{img.name}.png")
+
 
--- Задача 5/proverka.py
+++
@@ -1,3 +0,0 @@
-from PIL import Image
-img1 = Image.open("C:/Users/Admin/Documents/GitHub/Project-python-tasks/Задача 5/Image.png")
-img2 = Image.open("C:/Users/Admin/Documents/GitHub/Project-python-tasks/Задача 5/coded_inage.png")
