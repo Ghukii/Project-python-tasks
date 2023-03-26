1291269026a3beccd17beb87e589e9aff16d075c
Ghukii <68664943+Ghukii@users.noreply.github.com>
2023-03-26 22:21:06 +0300

    Task 5

--- Task 5/app.py
+++ Task 5/app.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from tkinter import messagebox
 from tkinter import filedialog as fd 
+from main import *
+
 
 window = Tk()
 window.title("Стеганографическое приложение")
@@ -18,24 +20,17 @@ Image_path_ask = Label(
 )
 Image_path_ask.grid(row=1, column=1)
 
-def get_path_from_file():
-    with open('Task 5/path.txt', 'r') as file:
-        path = file.read()
-    file.close()
-    return path
-
-Image_path = Label(
-    frame,
-    text = get_path_from_file(),
-)
-Image_path.grid(row=1, column=1)
-
 def callback():
     path = fd.askopenfilename()
-    with open('Task 5/path.txt', 'w') as file:
+    print(path)
+    with open("Task 5/path.txt", 'w') as file:
         file.write(path)
     file.close()
-    print(path)
+    Image_path = Label(
+    frame,
+    text = path
+    )
+    Image_path.grid(row=1, column=2)
 
 
 get_image_path = Button(
@@ -45,5 +40,67 @@ get_image_path = Button(
 )
 get_image_path.grid(row=1, column=3)
 
+space = Label(
+    frame,
+    text='                                                      '
+)
+space.grid(row=2,column=2)
+space.grid(row=3,column=2)
+
+get_phrase_ask = Label(
+    frame,
+    text="Введите текст который хотите зашифровать:"
+)
+get_phrase_ask.grid(row=4,column=1)
+
+get_phrase = Entry(
+    frame,
+    width=70
+)
+get_phrase.grid(row=4, column=2)
+
+def write_phrase_in_file():
+    phrase = get_phrase.get()
+    with open("Task 5/phrase.txt", 'w') as file:
+        file.write(phrase)
+    file.close()
+
+def read_phrase_in_file():
+    with open("Task 5/phrase.txt", 'r') as file:
+        phrase = file.read()
+    file.close()
+    return phrase
+
+enter_button = Button(
+    frame,
+    text = 'Подтвердить',
+    command=write_phrase_in_file
+)
+enter_button.grid(row=4, column=3)
+
+space.grid(row=6)
+space.grid(row=7)
+
+def get_path_from_file():
+    with open("Task 5/path.txt", 'r') as file:
+        path = file.read()
+    file.close()
+    return path
+
+def zashifrovat():
+    path = get_path_from_file()
+    phrase = str(read_phrase_in_file())
+    img = get_image(path)
+    phrase = phrase_mod(phrase)
+    delta = get_delta(phrase)
+    res = shifr(img, delta)
+    save_img(res)
+
+zashifrovat_button = Button(
+    frame,
+    text="Зашифровать",
+    command=zashifrovat
+)
+zashifrovat_button.grid(row=8, column=2)
 window.mainloop()
 
--- Task 5/main.py
+++ Task 5/main.py
@@ -1,24 +1,24 @@
 from PIL import Image
 
-img = Image.open("C:/Users/Admin/Documents/GitHub/Project-python-tasks/Задача 5/Image.png")
-
-print("Введите фразу для шифрования:")
-phrase = list(map(lambda x: bin(ord(x))[2:], input()))
+def get_image(path):
+    img = Image.open(path)
+    return img
 
 def parameter(x):
     if len(x) < 8:
-        print(x)
         x = ('0' * (8 - len(x))) + x
-        print(x)
     return x
 
-phrase = list(map(lambda x: parameter(x), phrase))
+def phrase_mod(phrase):
+    phrase = list(map(lambda x: bin(ord(x))[2:], phrase))
+    phrase = list(map(lambda x: parameter(x), phrase))
+    return phrase
 
-print(phrase)
-delta = list()
-for i in phrase:
-    for j in range(0, len(i)-2, 2):
-        delta.append(i[j] + i[j + 1])
+def get_delta(phrase):
+    delta = list()
+    for i in phrase:
+        for j in range(0, len(i)-2, 2):
+            delta.append(i[j] + i[j + 1])
 
 def rgb_to_hex(r, g, b):
     return ('{:x}{:x}{:x}').format(r, g, b)
@@ -26,7 +26,7 @@ def rgb_to_hex(r, g, b):
 def hex_to_rgb(hex):
     return tuple(int(hex[i:i+2], 16) for i in (2, 4, 6))
 
-def shifr(delta):
+def shifr(img, delta):
     width, height = img.size
     x = 0
     y = 0
@@ -43,6 +43,8 @@ def shifr(delta):
     return img
 
 def save_img(img):
-    img.save(f"coded_{img.name}.png")
+    img.save(f"Task 5/coded_image.png")
+
+
 
 
